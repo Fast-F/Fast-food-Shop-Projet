eb1fde844f498d2e3f32339a1766d4abb33f970f
lulemba-243 <65821626+lulemba-243@users.noreply.github.com>
2020-05-24 03:16:24 +0300

    Add files via upload

---
+++ Fast-food-Shop-Projet/git/Fast food Shop/Admn and User LogIn/Admn and User LogIn/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Admn_and_User_LogIn
+{
+    partial class AdmnAndUserLogIn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AdmnAndUserLogIn));
+            this.AdmnAndUserLoginGr = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.AdmnAndUserLoginGr.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // AdmnAndUserLoginGr
+            // 
+            this.AdmnAndUserLoginGr.BackColor = System.Drawing.Color.Transparent;
+            this.AdmnAndUserLoginGr.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("AdmnAndUserLoginGr.BackgroundImage")));
+            this.AdmnAndUserLoginGr.Controls.Add(this.pictureBox2);
+            this.AdmnAndUserLoginGr.Controls.Add(this.pictureBox1);
+            this.AdmnAndUserLoginGr.Location = new System.Drawing.Point(12, 12);
+            this.AdmnAndUserLoginGr.Name = "AdmnAndUserLoginGr";
+            this.AdmnAndUserLoginGr.Size = new System.Drawing.Size(1037, 537);
+            this.AdmnAndUserLoginGr.TabIndex = 0;
+            this.AdmnAndUserLoginGr.TabStop = false;
+            this.AdmnAndUserLoginGr.Text = "Admn and User Login";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.Location = new System.Drawing.Point(-11, -375);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(268, 247);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.pictureBox2.InitialImage = null;
+            this.pictureBox2.Location = new System.Drawing.Point(522, 195);
+            this.pictureBox2.MinimumSize = new System.Drawing.Size(100, 100);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(253, 190);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // AdmnAndUserLogIn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1061, 549);
+            this.Controls.Add(this.AdmnAndUserLoginGr);
+            this.Name = "AdmnAndUserLogIn";
+            this.Text = "Admn and User LogIn";
+            this.AdmnAndUserLoginGr.ResumeLayout(false);
+            this.AdmnAndUserLoginGr.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox AdmnAndUserLoginGr;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Fast-food-Shop-Projet/git/Fast food Shop/Admn and User LogIn/Admn and User LogIn/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Admn_and_User_LogIn
+{
+    public partial class AdmnAndUserLogIn : Form
+    {
+        public AdmnAndUserLogIn()
+        {
+            InitializeComponent();
+        }
+    }
+}
